9cb70410a90d012e8e4a3e1e5dab88dec9c44769
cch916 <33336602+cch916@users.noreply.github.com>
2017-12-14 10:00:04 +0800

    放弃了

--- src/main/java/com/test/controller/test.java
+++ src/main/java/com/test/controller/test.java
@@ -1,11 +1,15 @@
 package com.test.controller;
 
+import com.test.model.user;
 import com.test.service.testService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import javax.annotation.Resource;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/test")
@@ -13,14 +17,18 @@ public class test {
     @Resource
     private testService testService;
     @RequestMapping("/test")
-    public List  test() {
-        List<test> list=new ArrayList();
+    public Map test() {
+        Map map=new HashMap();
+        map.put("status","0");
+        List<user> list=new ArrayList();
         try {
             list = testService.getList();
+            map.put("list",list);
+            map.put("status","1");
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return list;
+        return map;
     }
 
 
--- src/main/webapp/static/test.js
+++ src/main/webapp/static/test.js
@@ -1,11 +1,14 @@
+$(function(){
+        $.get(url.test, function (data) {
+            if(data.status=="1"){
+                var obj=data.list;
+                for (var i=0;i<obj.length;i++)
+                {
+                    $("body").append("<h2>"+obj[i].username+"</h2>"+"<h2>"+obj[i].password+"</h2>");
+                }
+            }
 
-var test = function(){
-
-    $.get(url.test, function(data){
-            var josn=eval(data);
-            $("body").append("<h2>" + josn.username + "</h2>" + "<h2>" + josn.password + "</h2>")
-
-    });
-};
+        });
+});
 
 
